ab24354d5321b8212c180ff43d190f025a24bff8
Michiel Baird <michielbaird@gmail.com>
2012-08-08 09:48:15 +0200

    moving

--- datacontroller/web/management/commands/add_job.py
+++ datacontroller/web/management/commands/add_job.py
@@ -1,13 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
-from web.models import Job
+from web.models import Job, Site
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    args="job_file"
-    help="later"
+    args="assignee site type"
+    help    ="later"
 
     def handle(self, *args, **options):
-        user = User.objects.get(username="michiel")
+        username = args[0]
+        sitname = args[1]
+        job_type = args[2]
+        user = User.objects.get(username=username)
+        site = S
         j = Job(name="CLEANLOL", assignee=user,job_type="1",job_status="1",description="LOL")
         j.save()
         self.stdout.write("LOL\n")
--- datacontroller/web/models.py
+++ datacontroller/web/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Site(models.Model):
+    name = CharField(max_lengh=30)
 # Create your models here.
 class Job(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,primary_key=True)
+    site = models.ForgeinKey(Site)
     assignee = models.ForeignKey(User)
     job_type = models.CharField(max_length=1, choices=(('1', 'CLEAN'),
                                                        ('2', 'REGISTER')))
@@ -12,5 +15,7 @@ class Job(models.Model):
                                                          ('2', 'INPROGRESS'),
                                                          ('3', 'DONE')))
     description = models.TextField()
+
+
 
 
